7aa66852b84fabe58c7f1fbc06e4d37d1312178e
apzakharov <apzakharov9881@gmail.com>
2024-03-13 15:05:24 +0300

    Примеры методов в Java

--- src/main/java/org/hexlet/MyFunctionalInterface.java
+++ src/main/java/org/hexlet/MyFunctionalInterface.java
@@ -1,9 +1,23 @@
 package org.hexlet;
+
 import java.lang.FunctionalInterface;
+
 @FunctionalInterface
 public interface MyFunctionalInterface {
 
     // один абстрактный метод
     double getValue();
+
+    class ClassWithLambda {
+
+        private static double getValueFromLambda(MyFunctionalInterface lambdaMethod) {
+            return lambdaMethod.getValue();
+        }
+
+        public static void main(String[] args) {
+            double valueFromLambda = getValueFromLambda(() -> 10.0);
+            System.out.println(valueFromLambda);
+        }
+    }
 }
 
--- src/main/java/org/hexlet/RectangleInterface.java
+++ src/main/java/org/hexlet/RectangleInterface.java
@@ -1,19 +1,19 @@
 package org.hexlet;
 
+
 public interface RectangleInterface {
 
     default int calculateRectangleArea(int x, int y) {
         return x * y;
     }
 
+
     class Rectangle extends AbstractShape implements RectangleInterface {
         private int x, y;
 
         @Override
-        protected int calculateArea() {
+        public int calculateArea() {
             return calculateRectangleArea(x, y);
         }
-
-
     }
 }
